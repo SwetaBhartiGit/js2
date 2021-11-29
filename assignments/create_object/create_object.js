b68834c7e1be8b828a121059ff5a64c9245aa0e7
sweta bharti <swetasily@gmail.com>
2021-11-29 16:38:11 +0530

    create_object summision

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,9 @@
 function CreateObject(arr) {
     // Write your code here
+    var result={}
+    arr.forEach(function(t) { result[t[0]] =t[1]; });
+    return result;
+
 }
 
 module.exports = CreateObject;
